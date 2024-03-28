d46cb9097bd346c457858798199e5db2c7f2c365
shaporen <shaporen@yandex.ru>
2024-03-28 22:00:19 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,28 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+
+public class CyclicIterator<T> implements Iterator<T> {
+    private List<T> data;
+    private int index;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!data.isEmpty() && data.size() == index){
+            index = 0;
+        }
+        return !data.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data.get(index++);
+    }
+}
