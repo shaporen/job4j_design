757fc841a912d7094fd77285b5336c42e4a4a65b
shaporen <shaporen@yandex.ru>
2024-05-22 22:49:20 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,18 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public interface Tree<E> {
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+}
