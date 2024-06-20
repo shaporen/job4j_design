870a36b8f8cc98dec72850b0ccd0e086d2e1dd85
shaporen <shaporen@yandex.ru>
2024-06-20 06:58:52 +0300

    5.1. BufferedReader [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+
+public class LogFilter {
+    private final String file;
+
+    public LogFilter(String file) {
+        this.file = file;
+    }
+
+    public List<String> filter() {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader input = new BufferedReader(new FileReader(file))) {
+            for (String line = input.readLine(); line != null; line = input.readLine()) {
+                String[] parts = line.split(" ");
+                int length = parts.length;
+                if (length > 0 && "404".equals(parts[length - 2])) {
+                    list.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter("data/log.txt");
+        logFilter.filter().forEach(System.out::println);
+
+    }
+}
---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,13 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (BufferedReader input = new BufferedReader(new FileReader("data/input.txt"))) {
+            input.lines().forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
