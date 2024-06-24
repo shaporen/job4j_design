6c99bacda8e1af2c0810027b25819b19a5090e06
shaporen <shaporen@yandex.ru>
2024-06-24 21:21:14 +0300

    9.2. Атрибуты файла [#504941 #502218]

--- src/main/java/ru/job4j/io/PathExample.java
+++ src/main/java/ru/job4j/io/PathExample.java
@@ -2,30 +2,23 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.*;
 
 public class PathExample {
     public static void main(String[] args) throws IOException {
-        Path directory = Paths.get("path/paths");
-//        Files.createDirectories(directory);
-        Path path = Path.of("path/paths/path.txt");
-//        Files.createFile(path);
-        Files.move(path, Path.of("path/path.txt"));
-        Path target = Paths.get("path");
-        Path pathOne = Path.of("path/paths/path1.txt");
-        Files.createFile(pathOne);
-        Path pathTwo = Path.of("path/path2.txt");
-        Files.createFile(pathTwo);
-        DirectoryStream<Path> paths = Files.newDirectoryStream(target);
-        paths.forEach(System.out::println);
-//        System.out.println("Файл/директория существует?: " + Files.exists(path));
-//        System.out.println("Это директория?: " + Files.isDirectory(path));
-//        System.out.println("Это файл?: " + Files.isRegularFile(path));
-//        System.out.println("Имя файла: " + path.getFileName());
-//        System.out.println("Путь к файлу абсолютный?: " + path.isAbsolute());
-//        System.out.println("Родительская директория файла: " + path.getParent());
-//        System.out.println("Абсолютный путь к файлу: " + path.toAbsolutePath());
-//        System.out.println("Абсолютный путь к директории: " + directory.toAbsolutePath());
-//        System.out.println("Доступен для чтения?: " + Files.isReadable(path));
-//        System.out.println("Доступен для записи?: " + Files.isWritable(path));
+        Path file = Path.of("Attributes.txt");
+        //Files.createFile(file);
+        BasicFileAttributeView attrView = Files.getFileAttributeView(file,
+                BasicFileAttributeView.class);
+        BasicFileAttributes attributes = attrView.readAttributes();
+        System.out.println("Это обычный файл? " + attributes.isRegularFile());
+        System.out.println("Это директория? " + attributes.isDirectory());
+        System.out.println("Это символическая ссылка? " + attributes.isSymbolicLink());
+        System.out.println("Это не файл, директория или символическая ссылка? "
+                + attributes.isOther());
+        System.out.println("Дата создания файла: " + attributes.creationTime());
+        System.out.println("Размер файла: " + attributes.size());
+        System.out.println("Время последнего доступа: " + attributes.lastAccessTime());
+        System.out.println("Время последнего изменения: " + attributes.lastModifiedTime());
     }
 }
