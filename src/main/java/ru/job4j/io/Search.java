2ed62806a8993a910929ac8d88a67f2805983fd8
shaporen <shaporen@yandex.ru>
2024-06-28 15:44:24 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,6 +7,7 @@ import java.util.function.*;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        validateArgs(args);
         Path start = Paths.get(args[0]);
         search(start, path -> path.toFile().getName().endsWith(args[1]))
                 .forEach(System.out::println);
@@ -16,5 +17,18 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validateArgs(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Your arguments isn't valid");
+        }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException(args[0] + " isn't directory");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Еhe extension of "
+                    + "the file you are searching for should start with \".\"");
+        }
     }
 }
