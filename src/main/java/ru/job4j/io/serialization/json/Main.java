819900d8cba446aed55a8bee1c446d49d9bcb766
shaporen <shaporen@yandex.ru>
2024-07-19 22:15:49 +0300

    2. Формат JSON [#313164]

--- src/main/java/ru/job4j/io/serialization/json/Main.java
+++ src/main/java/ru/job4j/io/serialization/json/Main.java
@@ -5,7 +5,7 @@ import com.google.gson.*;
 public class Main {
     public static void main(String[] args) {
         final Company company = new Company(true, 5, new Contact("60-59-72"),
-                new String[] {"Trader", "Agroholding", "Elevator"});
+                new String[] {"Trader", "Agroholding"});
 
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(company));
@@ -19,7 +19,7 @@ public class Main {
                         + "\"phone\":\"60-59-72\""
                         + "},"
                         + "\"fieldsOfActivity\":"
-                        + "[\"Trader\",\"Agroholding\",\"Elevator\"]"
+                        + "[\"Trader\",\"Agroholding\"]"
                         + "}";
 
         final Company companyMod = gson.fromJson(companyJson, Company.class);
